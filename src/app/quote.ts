532349d7dc171ecb0f04a716c6a9c996c97d27d2
Bella Amandine <bellamandy45@gmail.com>
2020-11-01 21:34:22 +0200

    Add quote-details component

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -3,7 +3,7 @@ export class Quote {
     numberOfUpvote: number;
     numberOfDownvote: number;
 
-    constructor(public id:number, public fullname:string, public quote:string,public author:string, public createdDate: Date){
+    constructor(public id:number, public fullname:string, public quoteTitle:string, public author:string, public createdDate: Date){
         this.numberOfUpvote = 0;
         this.numberOfDownvote = 0;
     }
